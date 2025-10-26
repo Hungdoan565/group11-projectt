05856a9b279361abdd9ae1177fb6402c4aba747d
tranthanhhuy <tranthanhhuykaka@gmail.com>
2025-10-26 13:30:20 +0700

    database-add-POST/GET-api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+app.use(express.json());
+
+const userRoutes = require('./routers/users.js');
+app.use('/api', userRoutes);
+
+// Start the server
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
